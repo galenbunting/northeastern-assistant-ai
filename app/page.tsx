f507a8cc1e0c358a091a36a4f7faa21a9f3c5e63
galenbunting <46544983+galenbunting@users.noreply.github.com>
2026-02-18 14:46:11 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,9 @@ import Textarea from "react-textarea-autosize";
 import Image from "next/image";
 
 const examples = [
-  "Example 1",
-  "Example 2",
-  "Example 3",
+  "What is the taste of history?",
+  "what is inside you?",
+  "what is longing?",
 ];
 
 export default function Chat() {
@@ -83,7 +83,7 @@ export default function Chat() {
               className="h-20 w-20"
             />
             <h1 className="text-lg font-semibold text-black">
-              Hi, I'm a fine tuned LLM.
+              I'm a curious wandering LLM.
             </h1>
             <p className="text-gray-500">
               I'm part of a series of computational experiments taught by{" "}
